1fbad39c42cbc727f56f7c4b995853beb44bbdb7
demir <demir@FURKAN>
2025-04-29 22:07:43 +0300

    Add project files.

---
+++ IdentityChatEmailNightt/Context/EmailContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityChatEmailNightt.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatEmailNightt.Context
+{
+    public class EmailContext: IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=FURKAN;initial Catalog=EmailChatNightDb; integrated security=true;trust server certificate=true;");
+        }
+    }
+}
+
---
+++ IdentityChatEmailNightt/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using IdentityChatEmailNightt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityChatEmailNightt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
